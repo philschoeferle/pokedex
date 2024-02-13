43a8e4ea5813482f950bf2cd4ac824f3a335d93d
philschoeferle <philipp.schoeferle@web.de>
2024-02-13 09:22:36 +0100

    name and image

---
+++ script.js
@@ -0,0 +1,18 @@
+let currentPokemon;
+
+async function loadPokemon() {
+  let url = 'https://pokeapi.co/api/v2/pokemon/gengar';
+  let response = await fetch(url);
+  currentPokemon = await response.json();
+
+  renderPokemonInfo();
+  pokemonImage();
+}
+
+function renderPokemonInfo() {
+  document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
+}
+
+function pokemonImage() {
+  document.getElementById('pokemonImage').src = currentPokemon['sprites']['front_default'];
+}
